4a365df3810bb9e4cb46b643a5d89455b3fe9ca8
IncineratorG <alexander.v.dorohov@yandex.ru>
2021-12-09 21:36:53 +0300

    Work in progress.

--- lib/screens/notes_list/controllers/notes_list_main_controller.dart
+++ lib/screens/notes_list/controllers/notes_list_main_controller.dart
@@ -2,10 +2,12 @@ import 'package:notes_flutter/screens/notes_list/controllers/header/notes_list_h
 import 'package:notes_flutter/screens/notes_list/controllers/sort_options_menu/notes_list_sort_options_menu_controller.dart';
 
 class NotesListMainController {
-  final NotesListHeaderController _headerController = NotesListHeaderController();
-  final NotesListSortOptionsMenuController _sortOptionsMenuController = NotesListSortOptionsMenuController();
+  final NotesListHeaderController _headerController;
+  final NotesListSortOptionsMenuController _sortOptionsMenuController;
 
-  NotesListMainController();
+  NotesListMainController() :
+        _headerController = NotesListHeaderController(),
+        _sortOptionsMenuController = NotesListSortOptionsMenuController();
 
   NotesListHeaderController get headerController => _headerController;
   NotesListSortOptionsMenuController get sortOptionsMenuController => _sortOptionsMenuController;
